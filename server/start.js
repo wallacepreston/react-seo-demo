591178248bc4573b71a2bc6483dc5b8aad9ccbdf
Preston Wallace <wallace.preston@gmail.com>
2018-12-28 01:33:28 -0800

    edited start.js to remove db

--- server/start.js
+++ server/start.js
@@ -2,10 +2,10 @@ const {db} = require('./db');
 const app = require('./app');
 const port = process.env.PORT || 4000;
 
-db.sync({force: false})  // sync our database
-  .then(function(){
-    console.log('db synced')
+// db.sync({force: false})  // sync our database
+//   .then(function(){
+//     console.log('db synced')
     app.listen(port, () => console.log(`cookin' up good stuff on port ${port}`)) // then start listening with our express server once we have synced
-  })
+  // })
 
 module.exports = db
